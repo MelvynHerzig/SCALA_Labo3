4514cc4fe8773fc6b70f339ecfe4daf9764c6d87
Herzig Melvyn <melvyn.herzig@heig-vd.ch>
2022-05-19 15:04:19 +0200

    Fonctionne

--- src/main/scala/Web/StaticRoutes.scala
+++ src/main/scala/Web/StaticRoutes.scala
@@ -7,11 +7,11 @@ class StaticRoutes()(implicit val log: cask.Logger) extends cask.Routes :
   // TODO - Part 3 Step 1: Make the resources files (`.js` and `.css`) available to the browser.
   //      Do not forget to link to them from your HTML.
 
-  @cask.staticFiles("/static/css")
-  def staticCssFile() = "src/main/resources/css"
+  @cask.staticResources("/static/css")
+  def staticCssFile() = "./css"
 
-  @cask.staticFiles("/static/js")
-  def staticJsFile() = "src/main/resources/js"
+  @cask.staticResources("/static/js")
+  def staticJsFile() = "./js"
 
   initialize()
 
